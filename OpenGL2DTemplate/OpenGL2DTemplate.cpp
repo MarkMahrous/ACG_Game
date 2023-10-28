4b3f707ce68d032c88be68d75c599ed1ff3f9467
MarkMahrous <102079589+MarkMahrous@users.noreply.github.com>
2023-10-28 20:53:25 +0200

    including essential libraries and Draw The player and Circle to be used later

--- OpenGL2DTemplate/OpenGL2DTemplate.cpp
+++ OpenGL2DTemplate/OpenGL2DTemplate.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
 #include <glut.h>
+#include <ctime>
 
+int moveX = 500;
+int moveY = 300;
+
+int direction = GLUT_KEY_RIGHT;
 
 //this is the method used to print text in OpenGL
 //there are three parameters,
@@ -24,7 +30,61 @@ void print(int x, int y, char* string)
 	}
 }
 
+//DRAW Circle
+void drawCircle(int x, int y, float r) {
+	glPushMatrix();
+	glTranslatef(x, y, 0);
+	GLUquadric* quadObj = gluNewQuadric();
+	gluDisk(quadObj, 0, r, 50, 50);
+	glPopMatrix();
+}
 
+// DRAW Player
+void drawPlayer() {
+	glPointSize(5);
+	glPushMatrix();
+	glColor3f(0, 0, 1);
+	glTranslated(moveX, moveY, 0);
+	glScaled(1.3, 1.3, 0);
+	glRotated(0, 0, 0, 1);
+
+	if (direction == GLUT_KEY_RIGHT)
+		glRotated(0, 0, 0, 1);
+	if (direction == GLUT_KEY_UP)
+		glRotated(90, 0, 0, 1);
+	if (direction == GLUT_KEY_LEFT)
+		glRotated(180, 0, 0, 1);
+	if (direction == GLUT_KEY_DOWN)
+		glRotated(270, 0, 0, 1);
+
+	glColor3f(0.2, 0.2, 0.7);
+	glBegin(GL_QUADS);
+	glVertex3f(-10.0f, -10.0f, 0.0f);
+	glVertex3f(10.0f, -10.0f, 0.0f);
+	glVertex3f(10.0f, 10.0f, 0.0f);
+	glVertex3f(-10.0f, 10.0f, 0.0f);
+	glVertex3f(-10.0f, -10.0f, 0.0f);
+	glEnd();
+	glColor3f(1, 0, 0);
+	glBegin(GL_LINES);
+	glVertex3f(-10.0f, -10.0f, 0.0f);
+	glVertex3f(10.0f, 10.0f, 0.0f);
+	glVertex3f(10.0f, -10.0f, 0.0f);
+	glVertex3f(-10.0f, 10.0f, 0.0f);
+	glEnd();
+	glColor3f(0.1, 0.1, 0.8);
+	glBegin(GL_TRIANGLES);
+	glVertex3f(10.0f, -10.0f, 0.0f);
+	glVertex3f(10.0f, 10.0f, 0.0f);
+	glVertex3f(30.0f, 0.0f, 0.0f);
+	glEnd();
+	glColor3f(1, 1, 1);
+	glBegin(GL_POINTS);
+	glVertex3d(20, 0, 0);
+	glEnd();
+
+	glPopMatrix();
+}
 
 void Display() {
 	glClear(GL_COLOR_BUFFER_BIT);
